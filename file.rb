bf820aa2fd37bd89dd4e31cd8471656821355c86
saltsphinx <tremar.thomas19@gmail.com>
2021-07-01 16:18:44 -0500

    Fixed a few issues.

--- file.rb
+++ file.rb
@@ -1,7 +1,7 @@
 class String
     def sub_strings (array)
         array.reduce(Hash.new(0)) { |hash, string|
-            hash[string.to_s] += self.scan(string).length
+            hash[string.to_s] += self.scan(string).length unless string.scan(word) == []
             hash
         }
     end
@@ -10,7 +10,7 @@ end
 class Array
     def sub_strings (string)
         self.reduce(Hash.new(0)) { |hash, word|
-            hash[word.to_s] += string.scan(word).length
+            hash[word.to_s] += string.scan(word).length unless string.scan(word) == []
             hash
         }
     end
@@ -18,7 +18,8 @@ end
 
 def sub_strings (string, array)
     array.reduce(Hash.new(0)) { |hash, word|
-        hash[string.to_s] += self.scan(string).length
+        hash[word.to_s] += string.scan(word).length unless string.scan(word) == []
         hash
     }
 end
+puts sub_strings("Howdy partner, sit down! How's it going?", ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"])
